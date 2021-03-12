f939316311c87e8537fca4bcbd05f5fcea220708
Shaswata Raha <shaswata1029@gmail.com>
2021-03-12 17:33:13 +0530

    added the authentication for google and facebook

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -1,34 +1,84 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import FacebookIcon from "@material-ui/icons/Facebook";
+import { GoogleLogin } from "react-google-login";
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+import { useHistory } from "react-router-dom";
 import "./Form.css";
 
+const googleSuccess = async (res) => {
+  const result = res?.profileObj;
+  const token = res?.tokenId;
+  console.log(result);
+  console.log(token);
+  console.log(result.imageUrl);
+};
+
+const googleError = () => {
+  alert("Google Sign In was unsuccessful. Try again later");
+};
+
+const responseFacebook = (res) => {
+  console.log(res);
+
+  const token = res?.accessToken;
+
+  console.log(token);
+  console.log(res?.picture.data.url);
+};
+
 const Form = () => {
+  let history = useHistory();
+  console.log(history);
   return (
     <div className="sign-in-container">
       <div className="sign-in-template">
         <h3>Sign in To go to Dashboard</h3>
-        <button className="btn btn-danger button-items">
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-google"
-              viewBox="0 0 16 16"
+
+        <GoogleLogin
+          clientId="795096196898-6dvubjtf1ankpbgullclnmhv695ibnnv.apps.googleusercontent.com"
+          render={(renderProps) => (
+            <button
+              className="btn btn-danger button-items"
+              onClick={renderProps.onClick}
             >
-              <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
-            </svg>
-          </span>
-          <span>Sign In with Google</span>
-        </button>
-        <button className="btn btn-primary button-items">
-          <span>
-            <FacebookIcon />
-          </span>
-          <span>Sign In with Facebook</span>
-        </button>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-google"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
+                </svg>
+              </span>
+              <span>Sign In with Google</span>
+            </button>
+          )}
+          onSuccess={googleSuccess}
+          onFailure={googleError}
+          cookiePolicy="single_host_origin"
+        />
+
+        <FacebookLogin
+          appId="192298908937797"
+          autoLoad
+          callback={responseFacebook}
+          fields="name,email,picture"
+          render={(renderProps) => (
+            <button
+              className="btn btn-primary button-items"
+              onClick={renderProps.onClick}
+            >
+              <span>
+                <FacebookIcon />
+              </span>
+              <span>Sign In with Facebook</span>
+            </button>
+          )}
+        />
       </div>
     </div>
   );
