369e39f72aee7e763fa5aa68622579544db4db1b
Shaswata Raha <shaswata1029@gmail.com>
2021-03-12 11:36:59 +0530

    added the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
-import NavBar from './components/NavBar/NavBar';
-import{Route,Switch} from 'react-router-dom';
-import Form from './components/Form/Form';
-import './App.css';
+import NavBar from "./components/NavBar/NavBar";
+import { Route, Switch } from "react-router-dom";
+import Form from "./components/Form/Form";
+import Home from "./components/Home/Home";
+import "./App.css";
 
 function App() {
-    console.log("hello");
-    return (
+  console.log("hello");
+  return (
     <>
- <NavBar/>
- <Switch>
- <Route exact path='/'  component={NavBar}/>
- <Route exact path='/sign-in' component={Form}/>
- </Switch>
-</>
-    );
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/sign-in" component={Form} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,32 +1,43 @@
-import React from 'react';
-import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import {Link} from 'react-router-dom';
-import './NavBar.css';
+import React from "react";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = () => {
-    return (
-        <div>
-        <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark navbar-template">
-            <div className="template">
-              <div className="items">
-            <i class="fas fa-wallet"></i>
-            </div>
-            <div className="items">
-             Cash-Management Application
-            </div>
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark navbar-template">
+        <div className="template">
+          <div className="items">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-wallet"
+              viewBox="0 0 16 16"
+            >
+              <path d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5V3zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a1.99 1.99 0 0 1-1-.268zM1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1z" />
+            </svg>
           </div>
-          <div className="template">
-            <div>
-<Link to='/' exact  className="items">Home</Link>
-            </div>
-            <div>
-            <Link to='/sign-in' exact className="items"> Sign Up/Sign in</Link>
-            </div>
-            </div>
-  
-</nav>
-</div>
-    );
-}
+          <div className="items">Cash-Management Application</div>
+        </div>
+        <div className="template">
+          <div>
+            <Link to="/" exact className="items">
+              Home
+            </Link>
+          </div>
+          <div>
+            <Link to="/sign-in" exact className="items">
+              {" "}
+              Sign Up/Sign in
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default NavBar;
